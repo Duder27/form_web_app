b39175bc54fca7d536fc04061ff78d1f9a22e86f
Duder27 <77691123+Duder27@users.noreply.github.com>
2021-03-10 15:05:12 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,46 +1,58 @@
 from flask import Flask, url_for, render_template, request
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('q1t.html')
 
-@app.route("/response1")
-def render_response1():
-    response1 = request.args['r1'] 
-    if r1 == 'Chatty':
-        reply = "That's my name, too!"
+@app.route("/response")
+def render_response():
+    r1 = request.args['r1'] 
+    if r1 == 'Chattel':
+        reply = "That's my name too!!"
     else:
-        reply = "That's a very nice name. My name is Chatty."
-    return render_template('response1.html', r1 = reply)
+        reply = "My favorite color is pink."
+    return render_template('response.html', response = reply)
+    
+    
+@app.route("/")
+def render_main():
+    return render_template('q2t.html')
 
-@app.route("/response2")
-def render_response2():
-    response2 = request.args['r2'] 
+
+@app.route("/response")
+def render_response():
+    r2 = request.args['r2'] 
     if r2 == 'Yes':
-        reply = "I like the rain too."
+        reply = "I like rain too."
     else:
-        reply = "I love the rain!"
-    return render_template('response2.html', r2 = reply)
+        reply = "I love rain!"
+    return render_template('response.html', response = reply)
 
-@app.route("/response3")
-def render_response3():
-    response3 = request.args['r3'] 
+@app.route("/")
+def render_main():
+    return render_template('q3t.html')
+
+@app.route("/response")
+def render_response():
+    r3 = request.args['r3'] 
     if r3 == 'Spring':
-        reply = "Spring is my favorite season too."
+        reply = "Spring is also my favorite season"
     else:
         reply = "My favorite season is Spring."
-    return render_template('response3.html', r3 = reply)
+    return render_template('response.html', response = reply)
 
-@app.route("/response4")
-def render_response4():
-    response3 = request.args['r4'] 
-    if r3 == 'Chatty':
-        reply = "That's my name, too!"
+
+@app.route("/response")
+def render_response():
+    r4 = request.args['r4'] 
+    if r4 == 'Flight':
+        reply = "I would choose that one too. I would love the freedom."
     else:
-        reply = "That's a very nice name. My name is Chatty."
-    return render_template('response4.html', r4 = reply)
-    
+        reply = "Cool! I would choose flight because I would to be free to explore the world."
+    return render_template('response.html', response = reply)
+
+
 if __name__=="__main__":
     app.run(debug=False, port=54321)
